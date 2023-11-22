3ddd70c6eb9042630a3548422239c3c13e99dbbc
bhaveshmangal <bm6379@gmail.com>
2023-11-22 21:17:49 +0530

    Add requirements.py

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
+import users
+from users.models import Profile
+
 # Create your tests here.
+    # Profile object can be created with all required fields
+def test_create_profile_with_all_required_fields(self):
+    user = users.objects.create(username='test_user')
+    profile = Profile.objects.create(user=user)
+    assert profile.user == user
+    assert profile.name is None
+    assert profile.email is None
+    assert profile.profile_image is None
+    assert profile.username is None
+    assert profile.id is not None
