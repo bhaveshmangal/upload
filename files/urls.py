b3759830bab0e1f42131b8a0bdeff4f424bef89b
Bhavesh Mangal <bm6379@gmail.com>
2025-12-12 16:10:42 +0530

    Update urls.py

--- files/urls.py
+++ files/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('files/', views.myFiles, name="files"),
     path('sharedFiles/', views.sharedFiles, name="shared_files"),
     path('upload-file/', views.uploadFile, name="upload_file"),
-    # path('share-file/', views.share, name="share_form"),
+    path('share-file/', views.share, name="share_form"),
 ]
